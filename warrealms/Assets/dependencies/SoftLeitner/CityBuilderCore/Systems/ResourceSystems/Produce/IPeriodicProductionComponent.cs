f2c3522db0b7de28c323da0c29e5824cc3e27e40
gabrieldsvr <gabriel.vargas@paripassu.com.br>
2024-01-13 18:00:30 -0300

    Construcao com producao periodica

---
+++ warrealms/Assets/dependencies/SoftLeitner/CityBuilderCore/Systems/ResourceSystems/Produce/IPeriodicProductionComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CityBuilderCore
+{
+    /// <summary>
+    /// building component that produces items from other items
+    /// </summary>
+    public interface IPeriodicProductionComponent : IProgressComponent, IItemOwner
+    {
+        
+        /// <summary>
+        /// exposes the items the component can produce
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<ItemLevel> GetProducedItems();
+    }
+}
